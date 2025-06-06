3ce258974cbb8b52a821242d4b99ba517e504505
Ana <cladiaana2008@gmail.com>
2025-06-06 11:14:11 -0300

    Add files via upload

--- projeto-de-vida-main/projeto-de-vida-main/js-projeto-de-vida-codigo-base/js-projeto-de-vida-codigo-base/main.js
+++ projeto-de-vida-main/projeto-de-vida-main/js-projeto-de-vida-codigo-base/js-projeto-de-vida-codigo-base/main.js
@@ -15,7 +15,16 @@ for(let i=0;i < botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll (".contador");
-const tempoObjetivo1 = new Date("")
-const tempoObjetivo2 = new Date("")
-const tempoObjetivo3 = new Date("")
-const tempoObjetivo4 = new Date("")
+let tempoAtual = new Date();
+
+const tempoObjetivo1 = new Date("2025-12-12T00:00:00") - tempoAtual;
+contadores[0].textContent = tempoObjetivo1;
+
+const tempoObjetivo2 = new Date("2030-12-10T00:00:00") - tempoAtual;
+contadores[1].textContent = tempoObjetivo2;
+
+const tempoObjetivo3 = new Date("2033-09-10T00:00:00") - tempoAtual;
+contadores[2].textContent = tempoObjetivo3;
+
+const tempoObjetivo4 = new Date("2031-12-13T00:00:00") - tempoAtual;
+contadores[3].textContent = tempoObjetivo4;
